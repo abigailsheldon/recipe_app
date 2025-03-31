14b93c348906930e2675ceefbce9a8f2efc522a4
Devon  Ivory <devonivory@Devons-MacBook-Air.local>
2025-03-31 01:12:06 -0400

    added the submit button and put logic in to handle selecting recipes that you want to add to calendar

--- lib/meal_planner.dart
+++ lib/meal_planner.dart
@@ -1,119 +1,235 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart' show Provider;
 import 'package:recipe/db_helper.dart';
-import 'package:recipe/main.dart';
-import 'package:recipe/recipe_menu.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+
 class MealPlanner extends StatefulWidget {
   MealPlanner({super.key});
-  @override 
-  _Meal_PlannerState createState () => _Meal_PlannerState();
 
+  @override
+  _MealPlannerState createState() => _MealPlannerState();
 }
-class _Meal_PlannerState extends State<MealPlanner> {
-  List<Map<String,dynamic>> recipeNames = [];
+
+class _MealPlannerState extends State<MealPlanner> {
+  List<Map<String, dynamic>> recipeNames = [];
+  Map<DateTime, List<String>> mealPlan = {};
+  Map<String, bool> favoriteSelectedRecipe = {};
   late DateTime _selectedDay = DateTime.now();
+  List<String> checked = [];
+  late int recipeId;
   var _focusedDay = DateTime.now();
-  //late DatabaseHelper dbhelper ;
-  @override 
-void initState(){
+
+  @override
+  void initState() {
     super.initState();
-    final dbhelper = Provider.of<DatabaseHelper>(context, listen: false);
     _getAllRecipeData();
-    
-
   }
-void _getAllRecipeData() async{
-  // int count =  await dbHelper.numberOfRecipeName();
-    List<Map<String,dynamic>> allNames = await dbHelper.allRecipeNames();
 
+  void _favoritSelection(String recipe, bool? value) {
+    setState(() {
+      favoriteSelectedRecipe[recipe] = value ?? false;
+      if (value == true && !checked.contains(recipe)) {
+        checked.add(recipe);
+      } else if (value == false) {
+        checked.remove(recipe);
+      }
+    });
+    
+  }
+
+
+  void _clearSelection() {
   setState(() {
-    
-    //recipeNumber = count;
-    recipeNames = allNames;
+    checked.clear();
+    favoriteSelectedRecipe.clear();
   });
-      
-     
-       print("*****recipe count from db **********: ${recipeNames.length}");
-       print("Recipes: $recipeNames");
+}
+
+
+  void _getAllRecipeData() async {
+    final dbHelper = Provider.of<DatabaseHelper>(context, listen: false);
+    List<Map<String, dynamic>> allNames = await dbHelper.allRecipeNames();
+    setState(() {
+      recipeNames = allNames;
+    });
   }
 
+  Future<void> _getRecipeId(String recipe) async {
+    final dbHelper = Provider.of<DatabaseHelper>(context, listen: false);
+    int? id = await dbHelper.getRecipteNameById(recipe);
+    if (id != null) {
+      setState(() {
+        recipeId = id;
+      });
+    }
+  }
 
-@override
-Widget build(BuildContext context){
-  CalendarFormat _calendarFormat = CalendarFormat.month;
-  return Scaffold(
-    appBar: AppBar(
-      title: Text("Meal Planner"),
-    ),
-    body: SingleChildScrollView(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
+  Future<void> _updateRecipeDate(DateTime _status) async {
+    final dbHelper = Provider.of<DatabaseHelper>(context, listen: false);
+    
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: recipeId,
+      DatabaseHelper.date: _status.millisecondsSinceEpoch
+    };
+    final updatedRows = await dbHelper.updateFavoriteStatus(row);
+    print("Updated $updatedRows row(s)");
+  }
+  Future<void> _submit_Meal_Planner_Days()async{
+    //get the selected day 
+    //and the checked recipes
+    if(checked.isNotEmpty){
+        for(var checked_recipe in checked){
+          await _getRecipeId(checked_recipe);
+         await  _updateRecipeDate(_selectedDay);
+           ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Assigned $checked_recipe to  $_selectedDay'),
+        ),
+      );
+
+        }
+        _clearSelection();
+        
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    CalendarFormat _calendarFormat = CalendarFormat.month;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meal Planner"),
+      ),
+      body: Column(
+        children: [
+ Expanded(
+  child: DragTarget<Map<String, dynamic>>(
+    onAcceptWithDetails: (details) {
+      setState(() {
+        List<String> draggedRecipes = List<String>.from(details.data["recipe"]);
+
+        if (mealPlan[_selectedDay] == null) {
+          mealPlan[_selectedDay] = draggedRecipes;
+        } else {
+          mealPlan[_selectedDay]!.addAll(draggedRecipes);
+        }
+
+        for (var recipe in draggedRecipes) {
+          _getRecipeId(recipe);
+          _updateRecipeDate(_selectedDay);
+        }
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Assigned recipes ${details.data["recipe"]} to $_selectedDay'),
+        ),
+      );
+    },
+    builder: (context, candidateData, rejectedData) {
+      return SingleChildScrollView(
         child: Column(
           children: [
-             TableCalendar(
-                        firstDay: DateTime.utc(2010, 10, 16),
-                        lastDay: DateTime.utc(2030, 3, 14),
-                        focusedDay: _focusedDay,
-                        selectedDayPredicate: (day) {
-                          return isSameDay(_selectedDay, day);
-                        },
-                        onDaySelected: (selectedDay, focusedDay) {
-                          setState(() {
-                            _selectedDay = selectedDay;
-                            _focusedDay = focusedDay; // update `_focusedDay` here as well
-                          });
-                        },
-                        calendarFormat: _calendarFormat,
-                        onFormatChanged: (format) {
-                          setState(() {
-                            _calendarFormat = format;
-                          });
-                        },
-                      ),
-                    
-            Flexible(
-              child: ListView.builder(
-                  itemCount: recipeNames.length,
-                  itemBuilder: (context,index){
-                    return DragTarget<Map<String,dynamic>>(
-                      onAcceptWithDetails: (details) {
-                    setState(() {
-                      recipeNames[index][DatabaseHelper.date] = details.data["task"];
-                    });
-
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Assigned ${details.data["task"]} to $_selectedDay'),
-                      ),
-                    );
-                    
-                  },
-                  builder: (context, candidateData, rejectedData) {
-                    return Container(
-                     
-                      child: ListTile(
-                        title: Text(
-                          recipeNames[index][DatabaseHelper.columnName],
-                          selectionColor: Colors.red,)
-                          ,
-                      )
-                    );
-              
-                  }
-              );
-                  }
-              ),
-            )
-              
-
+            TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = DateTime(selectedDay.year,selectedDay.month,selectedDay.day);
+                  _focusedDay = focusedDay;
+                });
+              },
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+            ),
+            Text(
+              "Meals for ${_selectedDay.toLocal()}",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Wrap(
+              children: mealPlan[_selectedDay]?.map((recipe) {
+                    return Chip(label: Text(recipe));
+                  }).toList() ??
+                  [],
+            ),
+            
           ],
         ),
+      );
+    },
+  ),
+),
+
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: recipeNames.length,
+              itemBuilder: (context, index) {
+                String name = recipeNames[index][DatabaseHelper.columnName];
+                bool value = favoriteSelectedRecipe[name] ?? false;
+
+                return LongPressDraggable<Map<String, dynamic>>(
+                  data: {'recipe': checked.isNotEmpty ? checked : [name]},
+                  feedback: Material(
+                    color: Colors.black,
+                    child: Column(
+                      children: checked.isNotEmpty
+                          ? checked.map((r) => Text(
+                                r,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )).toList()
+                          : [
+                              Text(
+                                name,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                    ),
+                  ),
+                  childWhenDragging: Container(),
+                  child: ListTile(
+                    title: Text(name),
+                    leading: Checkbox(
+                      value: value,
+                      onChanged: (bool? newValue) {
+                        _favoritSelection(name, newValue);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(
+              children: [
+                ElevatedButton(onPressed: (){
+                  _submit_Meal_Planner_Days();
+                }, 
+                child: Text("Submit"))
+              ],
+            )
+        ],
         
       ),
-        
-      
-    ),
-  );
-}
+    );
+  }
 }
